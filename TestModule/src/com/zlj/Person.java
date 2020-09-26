51ed2e32398c444418f910bea8c8cbffabd58287
zhulinjian <ncdx2010@126.com>
2020-09-26 22:22:10 +0800

    新建Person类

---
+++ TestModule/src/com/zlj/Person.java
@@ -0,0 +1,13 @@
+package com.zlj;
+
+/**
+ * @Auther: Peter zhu
+ * @Date: 2020/9/26 - 09 - 26 - 22:20
+ * @Description: com.zlj
+ * @version: 1.0
+ */
+public class Person {
+    private String name;
+    private int age;
+    private double height;
+}
